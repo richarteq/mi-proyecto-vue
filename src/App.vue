c99f8a2f998d5c05a86faf396155b624e0183a71
Richart Escobedo <richarteq@gmail.com>
2025-04-20 23:18:23 -0500

    Directiva v-else. Error

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
   <h1>Hola {{ name }}!</h1>
   <h2 v-if="active === true">Estoy en true</h2>
   <h2 v-else-if="active === false">Estoy en false</h2>
+  <p>Lorem ipsum dolor sit</p>
   <h2 v-else>Estoy indeciso</h2>
 </template>
 
