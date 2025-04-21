62a281f8ca7c8ddc1ab2319244e3862fa1cb323d
Richart Escobedo <richarteq@gmail.com>
2025-04-20 22:49:57 -0500

    Uso de expresiones de JavaScript

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <script setup>
   const name = "Vue 3";
   const styleColor = "color: red;";
+  const active = true;
+  const colors = ["blue", "red", "green"];
+  const counter = 1;
 </script>
 
 <template>
   <h1>Hola {{ name }}!</h1>
-  <h2 :style="styleColor">Hola {{ name }}</h2>
+  <h2 :style="styleColor">Hola {{ name.toUpperCase() }}</h2>
+  <h3>{{ active ? "Estoy activado" : "Estoy desactivado" }}</h3>
+  <h4 :style="`color: ${colors[2]}`">Color con interpolación</h4>
+  <h5>Contador: {{ counter + 1 }}</h5>
 </template>
