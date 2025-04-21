9660484f9e43a26e47193ee1fd976803fbb932fe
Richart Escobedo <richarteq@gmail.com>
2025-04-20 22:46:24 -0500

    Enlaces de atributos

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
-  const name = 'Vue 3';
+  const name = "Vue 3";
+  const styleColor = "color: red;";
 </script>
 
 <template>
-  <h1>Hola {{name}}!</h1>
+  <h1>Hola {{ name }}!</h1>
+  <h2 v-bind:style="styleColor">Hola {{ name }}</h2>
 </template>
