0de090166152b1ce5f696c22240c58d005d44bf3
Richart Escobedo <richarteq@gmail.com>
2025-04-20 23:14:53 -0500

    Directiva v-else-if

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
   const name = "Vue 3";
-  const active = false;
+  const active = null;
 </script>
 
 <template>
   <h1>Hola {{ name }}!</h1>
-  <h2 v-if="active">v-if en acción</h2>
-  <h2 v-else>v-else en acción</h2>
+  <h2 v-if="active === true">Estoy en true</h2>
+  <h2 v-else-if="active === false">Estoy en false</h2>
+  <h2 v-else>Estoy indeciso</h2>
 </template>
+
