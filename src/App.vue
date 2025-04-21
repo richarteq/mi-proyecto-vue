796aa35cb6cead1db22630b4d0524e33f3158a40
Richart Escobedo <richarteq@gmail.com>
2025-04-20 23:20:32 -0500

    Directiva v-show

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,10 @@
 <script setup>
   const name = "Vue 3";
-  const active = null;
+  const active = true;
 </script>
 
 <template>
   <h1>Hola {{ name }}!</h1>
-  <h2 v-if="active === true">Estoy en true</h2>
-  <h2 v-else-if="active === false">Estoy en false</h2>
-  <p>Lorem ipsum dolor sit</p>
-  <h2 v-else>Estoy indeciso</h2>
+  <h2 v-show="active">Estoy en true</h2>
 </template>
 
