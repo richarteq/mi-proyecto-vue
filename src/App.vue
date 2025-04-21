da15b17e3e825eccc914d7b985ab6e091f1ee998
Richart Escobedo <richarteq@gmail.com>
2025-04-20 23:38:41 -0500

    Directiva v-for (En un Objeto)

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,18 @@
 <script setup>
   const name = "Vue 3";
-  const arrayFrutas = [
-    {
-      name: "Manzana",
-      price: "$1.00",
-      description: "Una manzana",
-    },
-    {
-      name: "Pera",
-      price: "$2.00",
-      description: "Una pera",
-    },
-    {
-      name: "Naranja",
-      price: "$3.00",
-      description: "Una naranja",
-    },
-  ];
+
+  const fruta = {
+    name: "Naranja",
+    price: "$3.00",
+    description: "Una naranja",
+  };
 </script>
 
 <template>
   <h1>Hola {{ name }}!</h1>
   <ul>
-    <li v-for="fruta in arrayFrutas" :key="name">
-      {{ fruta.name }} - {{ fruta.price }} - {{ fruta.description }}
+    <li v-for="(value, propiedad) in fruta">
+      {{ propiedad }} : {{ value }}
     </li>
   </ul>
 </template>
