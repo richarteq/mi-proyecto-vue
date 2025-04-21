497a2894ce13029ff748f7519eb8c54e78ec3495
Richart Escobedo <richarteq@gmail.com>
2025-04-21 01:53:21 -0500

    Práctica: Propiedades computadas en un arreglo

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@ import { ref, computed } from "vue";
 
 const name = "Vue 3";
 const counter = ref(0);
+const arrayCounter = ref([]);
+
 const increment = () => {
   counter.value++;
 };
@@ -12,23 +14,35 @@ const decrement = () => {
 const reset = () => {
   counter.value = 0;
 };
-// método
-const classCounter = () => {
+const add = () => {
+  arrayCounter.value.push(counter.value);
+};
+const blockNumber = computed(() => {
+  const number = arrayCounter.value.find((num) => num === counter.value);
+  return number || number === 0;
+});
+const classCounter = computed(() => {
   if (counter.value === 0) {
     return "zero";
   }
   return counter.value > 0 ? "positive" : "negative";
-};
+});
 </script>
 
 <template>
   <h1>Hola {{ name }}!</h1>
-  <h2 :class="classCounter()">
+  <h2 :class="classCounter">
     {{ counter }}
   </h2>
   <button @click="increment">Incremet</button>
   <button @click="decrement">Decrement</button>
   <button @click="reset">Reset</button>
+  <button @click="add" :disabled="blockNumber">Add</button>
+  <ul>
+    <li v-for="(item, index) in arrayCounter" :key="index">
+        {{ item }}
+    </li>
+  </ul>
 </template>
 
 <style>
