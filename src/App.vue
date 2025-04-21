08f2829011f71e9a90391072c51b95e174331868
Richart Escobedo <richarteq@gmail.com>
2025-04-20 23:03:38 -0500

    Directiva v-if

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,9 @@
 <script setup>
   const name = "Vue 3";
-  const styleColor = "color: red;";
   const active = true;
-  const colors = ["blue", "red", "green"];
-  const counter = 1;
 </script>
 
 <template>
   <h1>Hola {{ name }}!</h1>
-  <h2 :style="styleColor">Hola {{ name.toUpperCase() }}</h2>
-  <h3>{{ active ? "Estoy activado" : "Estoy desactivado" }}</h3>
-  <h4 :style="`color: ${colors[2]}`">Color con interpolación</h4>
-  <h5>Contador: {{ counter + 1 }}</h5>
-  <p>{{if(active) {return counter}}}</p>
+  <h2 v-if="active">v-if en acción</h2>
 </template>
